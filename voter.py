81eb6310d151867ed8fac0d2e4e97e8fe84379f9
ftred12 <122821658+ftred12@users.noreply.github.com>
2023-02-03 14:08:26 +0300

    Create voter.py

---
+++ voter.py
@@ -0,0 +1,8 @@
+a=("nationality")
+a=['Kenyan','Ugandan','Tanzanian']
+age=int(input("Enter the age:"))
+nationality=input("Enter the nationality:")
+if ((nationality == "a") and (age>=18)):
+    print(" not Eligible to vote")
+else:
+    print(" eligible to vote")
